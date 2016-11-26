69ca0630e006a8b00e1a8f412cc0e009bf3ba314
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2016-11-26 18:53:43 -0300

    Scripts for compile and test the metaheuristics added.

---
+++ src/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compiles TSPLIB4J
+javac org/moeaframework/problem/tsplib/*.java
+
+# Compiles out lib
+javac -cp .:org.moeaframework.problem.tsplib.* lib/*.java
+
+# Compiles the tester class
+javac -cp .:org.moeaframework.problem.tsplib.* main/*.java
