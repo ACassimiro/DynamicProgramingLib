6d613e5efaa2b8f940ffd20851ac7514756a1514
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2016-11-26 21:57:26 -0300

    Scripts to run metaheuristics modified.

---
+++ src/metaheuristics.run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp .:org.moeaframework.problem.tsplib.* main.Tester $1 $2 $3 $4
--- src/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp .:org.moeaframework.problem.tsplib.* main.Tester $1 $2 $3
